073aaac002a904eccd440669586d1b3ac3b7f0ef
Cliff Pyles <cliff@cliffpyles.com>
2023-04-07 06:35:07 -0400

    Add support for converting joi schema to json schema in schemas-cli

--- apps/schemas-cli/commands/convertSchema.mjs
+++ apps/schemas-cli/commands/convertSchema.mjs
@@ -1,8 +1,16 @@
 import Joi from 'joi'
 import fetch from 'node-fetch'
 import fs from 'fs/promises'
+import parse from 'joi-to-json'
 
-async function jsonSchemaToJoi (input, outputPath=null) {
+/**
+ * Converts a JSON Schema to a Joi schema configuration that can be used by Joi.compile()
+ *
+ * @param {string} input - URL to a valid JSON Schema
+ * @param {string} [outputPath] - Filepath where the converted Joi schema should be saved
+ * @returns {Object} converted Joi schema
+ */
+async function jsonSchemaToJoiSchema (input, outputPath = null) {
   async function fetchSchema (url) {
     try {
       const response = await fetch(url)
@@ -19,7 +27,7 @@ async function jsonSchemaToJoi (input, outputPath=null) {
     if (typeof schema === 'object' && schema !== null) {
       if (schema.$ref) {
         const refParts = schema.$ref.split('/')
-        console.log(refParts)
+
         if (refParts[0] === '#') {
           refParts.shift()
         }
@@ -45,19 +53,19 @@ async function jsonSchemaToJoi (input, outputPath=null) {
       throw new Error('Invalid JSON schema')
     }
 
-    const isRequired = (key) => requiredProperties.includes(key);
+    const isRequired = key => requiredProperties.includes(key)
 
     if (schema.type === 'object') {
       let joiSchema = Joi.object()
       if (schema.properties) {
-        const keys = {};
+        const keys = {}
         for (const key in schema.properties) {
-          keys[key] = convert(schema.properties[key], schema.required);
+          keys[key] = convert(schema.properties[key], schema.required)
           if (isRequired(key)) {
-            keys[key] = keys[key].required();
+            keys[key] = keys[key].required()
           }
         }
-        joiSchema = joiSchema.keys(keys);
+        joiSchema = joiSchema.keys(keys)
       }
       return joiSchema
     }
@@ -107,38 +115,108 @@ async function jsonSchemaToJoi (input, outputPath=null) {
     throw new Error(`Unsupported JSON schema type: ${schema.type}`)
   }
 
-  const isUrl = typeof input === 'string' && input.startsWith('http');
-  const jsonSchema = isUrl ? await fetchSchema(input) : input;
+  const isUrl = typeof input === 'string' && input.startsWith('http')
+  const jsonSchema = isUrl ? await fetchSchema(input) : input
 
-  const definitions = jsonSchema.definitions || {};
-  const resolvedSchema = resolveRef(jsonSchema, definitions);
+  const definitions = jsonSchema.definitions || {}
+  const resolvedSchema = resolveRef(jsonSchema, definitions)
 
-  const joiSchema = convert(resolvedSchema);
+  const joiSchema = convert(resolvedSchema)
 
   if (outputPath) {
     try {
-      await fs.writeFile(outputPath, JSON.stringify(joiSchema.describe(), null, 2));
-      console.log(`Joi schema saved to: ${outputPath}`);
+      await fs.writeFile(
+        outputPath,
+        JSON.stringify(joiSchema.describe(), null, 2)
+      )
+      console.log(`Schema saved to: ${outputPath}`)
     } catch (error) {
-      throw new Error(`Error writing Joi schema to file: ${error.message}`);
+      throw new Error(`Error writing Joi schema to file: ${error.message}`)
     }
   }
 
-  return joiSchema;
+  return joiSchema
 }
 
-async function convertSchema ({ output, source }) {
-  try {
-    const location = new URL(source)
-
-    if (!location?.href?.length) {
-      throw new Error('The --source argument must be a valid URL.')
+/**
+ * Converts Joi schema configuration (Joi.describe) to a JSON Schema
+ *
+ * @param {string} input - URL to a valid JSON Schema
+ * @param {string} [outputPath] - Filepath where the converted Joi schema should be saved
+ * @param {('json'|'open-api'|'json-draft-04'|'json-draft-2019-09')} [version] - Version of JSON Schema to use
+ * @returns {Object} converted JSON Schema
+ */
+async function joiSchemaToJsonSchema (input, outputPath = null, version = 'json') {
+  async function fetchSchema (url) {
+    try {
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw new Error(`Failed to fetch JSON schema: ${response.statusText}`)
+      }
+      return await response.json()
+    } catch (error) {
+      throw new Error(`Error fetching JSON schema: ${error.message}`)
     }
+  }
 
-    const joiSchema = await jsonSchemaToJoi(source, output)
+  const isUrl = typeof input === 'string' && input.startsWith('http')
+  const joiSchemaConfig = isUrl ? await fetchSchema(input) : input
+  const joiSchema = Joi.compile(joiSchemaConfig)
+  const jsonSchema = parse(joiSchema, version)
+
+  if (outputPath) {
+    try {
+      await fs.writeFile(outputPath, JSON.stringify(jsonSchema, null, 2))
+      console.log(`Schema saved to: ${outputPath}`)
+    } catch (error) {
+      throw new Error(`Error writing Joi schema to file: ${error.message}`)
+    }
+  }
+
+  return jsonSchema
+}
+
+async function convertSchema ({ output, outputType = 'json', source, sourceType = 'joi' }) {
+  try {
+    const type = `${sourceType}-to-${outputType}`
+    console.log(`Attempting ${type} conversion`)
+    switch (type) {
+      case 'json-to-joi':
+        const joiSchema = await jsonSchemaToJoiSchema(source, output)
+        break
+      case 'joi-to-json':
+        const jsonSchema = await joiSchemaToJsonSchema(
+          source,
+          output,
+          outputType
+        )
+        break
+      case 'joi-to-json-draft-04':
+        const jsonDraft04Schema = await joiSchemaToJsonSchema(
+          source,
+          output,
+          outputType
+        )
+        break
+      case 'joi-to-json-draft-2019-09':
+        const jsonDraft2019Schema = await joiSchemaToJsonSchema(
+          source,
+          output,
+          outputType
+        )
+        break
+      case 'joi-to-open-api':
+        const openApiSchema = await joiSchemaToJsonSchema(
+          source,
+          output,
+          outputType
+        )
+        break
+      default:
+        throw new Error(`Unknown conversion type (${type})`)
+    }
   } catch (error) {
-    // console.error('convertSchema: ', err)
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`)
   }
 }
 
--- apps/schemas-cli/schemas-cli.mjs
+++ apps/schemas-cli/schemas-cli.mjs
@@ -2,7 +2,7 @@
 
 process.env['SUPPRESS_NO_CONFIG_WARNING'] = true
 
-import { Command } from 'commander'
+import { Command, Option } from 'commander'
 
 import convertSchema from './commands/convertSchema.mjs'
 
@@ -17,6 +17,17 @@ program
   .description('Convert a schema')
   .option('--source <string>', 'Source to use for the schema rules')
   .option('--output <string>', 'Location to save output')
+  .addOption(
+    new Option('-st, --source-type <string>', 'Type of source')
+      .choices(['joi', 'json', 'json-draft-04', 'json-draft-2019-09', 'open-api'])
+      .default('json')
+  )
+  .addOption(
+    new Option('-ot, --output-type <string>', 'Type of output')
+      .choices(['joi', 'json', 'json-draft-04', 'json-draft-2019-09', 'open-api'])
+      .default('joi')
+  )
   .action(convertSchema)
+
 
 program.parse(process.argv)

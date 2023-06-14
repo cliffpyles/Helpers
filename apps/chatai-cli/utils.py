0d9b2cee7e9ce312500f7b75f8d5d2cdf7d3159b
Cliff Pyles <cliff@cliffpyles.com>
2023-06-14 07:20:42 -0400

    Add support for nested prompts in chatai-cli

--- apps/chatai-cli/utils.py
+++ apps/chatai-cli/utils.py
@@ -4,6 +4,7 @@ import json
 import openai
 import os
 import requests
+import glob
 import rich_click as click
 import shutil
 import socket
@@ -163,11 +164,14 @@ def load_prompt(prop_name="default", filepath=None):
 
 
 def load_prompts():
-    script_dir = Path(__file__).resolve(strict=False).parent
+    prompts_dir = Path(__file__).resolve(strict=False).parent / f"./prompts"
+    prompt_files = glob.glob("**/*.yaml", root_dir=prompts_dir, recursive=True)
+
     prompts = [
-        yaml.safe_load(prompt.read_text())
-        for prompt in script_dir.glob("./prompts/*.yaml")
+        yaml.safe_load(Path(prompts_dir / f"./{prompt}").read_text())
+        for prompt in prompt_files
     ]
+
     return prompts
 
 
@@ -179,19 +183,27 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
         print("Warning: model not found. Using cl100k_base encoding.")
         encoding = tiktoken.get_encoding("cl100k_base")
     if model == "gpt-3.5-turbo":
-        print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.")
+        print(
+            "Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301."
+        )
         return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
     elif model == "gpt-4":
-        print("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.")
+        print(
+            "Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314."
+        )
         return num_tokens_from_messages(messages, model="gpt-4-0314")
     elif model == "gpt-3.5-turbo-0301":
-        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_message = (
+            4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        )
         tokens_per_name = -1  # if there's a name, the role is omitted
     elif model == "gpt-4-0314":
         tokens_per_message = 3
         tokens_per_name = 1
     else:
-        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
     num_tokens = 0
     for message in messages:
         num_tokens += tokens_per_message
